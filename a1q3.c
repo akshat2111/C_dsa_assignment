54f05c07c5b923e04e1e390e7a617deaa8c46a37
akshat2111 <akv986214@gmail.com>
2024-08-22 09:53:32 +0530

    first commit

--- a1q3.c
+++ a1q3.c
@@ -11,46 +11,46 @@ typedef struct poly {
 } poly;
 
 
-void addPolynomials(poly* P1, poly* P2, poly* P3) {
+void addPolynomials(poly P1, poly P2, poly* P3) {
     int i = 0, j = 0, k = 0;
 
-    while (i < P1->no_of_terms && j < P2->no_of_terms) {
-        if (P1->a[i].expo == P2->a[j].expo) {
-            P3->a[k].coeff = P1->a[i].coeff + P2->a[j].coeff;
-            P3->a[k].expo = P1->a[i].expo;
-            if (P3->a[k].coeff != 0) { 
+    while (i < P1.no_of_terms && j < P2.no_of_terms) {
+        if (P1.a[i].expo == P2.a[j].expo) {
+            (*P3).a[k].coeff = P1.a[i].coeff + P2.a[j].coeff;
+            (*P3).a[k].expo = P1.a[i].expo;
+            if ((*P3).a[k].coeff != 0) { 
                 k++;
             }
             i++;
             j++;
-        } else if (P1->a[i].expo < P2->a[j].expo) {
-            P3->a[k] = P1->a[i];
+        } else if (P1.a[i].expo < P2.a[j].expo) {
+            (*P3).a[k] = P1.a[i];
             i++;
             k++;
         } else {
-            P3->a[k] = P2->a[j];
+            (*P3).a[k] = P2.a[j];
             j++;
             k++;
         }
     }
 
 
-    while (i < P1->no_of_terms) {
-        P3->a[k++] = P1->a[i++];
+    while (i < P1.no_of_terms) {
+        (*P3).a[k++] = P1.a[i++];
     }
 
-    while (j < P2->no_of_terms) {
-        P3->a[k++] = P2->a[j++];
+    while (j < P2.no_of_terms) {
+        (*P3).a[k++] = P2.a[j++];
     }
 
-    P3->no_of_terms = k;
+    (*P3).no_of_terms = k;
 }
 
-void printPolynomial(poly* p) {
+void printPolynomial(poly p) {
     int i;
-    for ( i = 0; i < p->no_of_terms; i++) {
-        printf("%.1fx^%d", p->a[i].coeff, p->a[i].expo);
-        if (i != p->no_of_terms - 1) {
+    for ( i = 0; i < p.no_of_terms; i++) {
+        printf("%.1fx^%d", p.a[i].coeff, p.a[i].expo);
+        if (i != p.no_of_terms - 1) {
             printf(" + ");
         }
     }
@@ -66,10 +66,10 @@ int main() {
     P3.no_of_terms = 0;
 
  
-    addPolynomials(&P1, &P2, &P3);
+    addPolynomials(P1, P2, &P3);
 
     printf("Resultant polynomial: ");
-    printPolynomial(&P3);
+    printPolynomial(P3);
 
     return 0;
 }
